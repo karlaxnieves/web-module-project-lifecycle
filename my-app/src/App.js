29b52b2d72162f0c6765ecd11d9feb07b4183d67
Karla Nieves <karlaxnieves@gmail.com>
2021-05-04 15:58:19 -0700

    did my axios call

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
+class App extends React.Component {
+state = {
+  user: {},
+  followers: {}
+}
+
+componentDidMount() {
+  axios.get('https://api.github.com/users/karlaxnieves')
+      .then(resp=> {
+          this.setState({
+              user: resp.data
+          });
+          console.log(resp.data)
+      })
+      .catch(err => {
+          console.log(err);
+      });
+}
+
+  render() {
   return (
     <div className="App">
-      
+      <h1>{this.state.user.login}</h1>
     </div>
   );
+}
 }
 
 export default App;
