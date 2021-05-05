6a7ce12096764fa3bbe35fa8755800c7ef3c0db9
Karla Nieves <karlaxnieves@gmail.com>
2021-05-04 19:01:30 -0700

    created a followers navigation bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,8 @@ import Followers from './Followers'
 
 class App extends React.Component {
 state = {
-  user: {}
+  user: {},
+  followers: ''
 }
 
 componentDidMount() {
@@ -19,12 +20,39 @@ componentDidMount() {
       });
 }
 
+handleChange = (e) =>{
+  this.setState({
+    followers: e.target.value
+  })
+}
+
+handleSubmit = (e) =>{
+  e.preventDefault();
+  axios.get(`https://api.github.com/users/${this.state.followers}`)
+  .then(res =>{
+    this.setState({
+      followers: res.data
+      })
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err);
+  })
+}
+
   render() {
   return (
     <div className="App">
+<h1>Navigate Users</h1>
+<form onSubmit={this.handleSubmit}>
+<input value={this.state.followers} onChange={this.handleChange}/>
+<button>Fetch Followers</button>
+</form>
+
+
       <div className="container">
       <img className="avatar" src={this.state.user.avatar_url} alt="avatar"/>
-      <h1>@{this.state.user.login}</h1>
+      <h3>@{this.state.user.login}</h3>
       <h3>Location: {this.state.user.location}</h3>
       <h3>Following: {this.state.user.following}</h3>
       <h3>Followers: {this.state.user.followers}</h3>
